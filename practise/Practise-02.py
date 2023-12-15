63d9376f274ad8a8049e42a8e506ff83a24643b4
jlin <jlin@justhealth.cn>
2023-12-15 15:40:45 +0800

    practise

---
+++ practise/Practise-02.py
@@ -0,0 +1,7 @@
+# 02-print 函数
+print('hello world!')
+print('hello', 'world!')  # 逗号自动添加默认的分隔符：空格 hello world!
+print('hello' + 'world!')  # 加号表示字符拼接 helloworld!
+print('hello', 'world', sep='***')  # 单词间用***分隔 hello***world
+print('#' * 50)  # *号表示重复 50 遍
+print('how are you?', end='')  # 默认 print 会打印回车，end=''表示不要回车
