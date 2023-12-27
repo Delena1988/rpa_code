607f0d64443a8d585c3eca284475f27be3840541
jlin <jlin@justhealth.cn>
2023-12-27 09:05:45 +0800

    practise-10

---
+++ practise/Practise-10.py
@@ -0,0 +1,19 @@
+# 10-基本判断
+if 3 > 0:
+    print('yes')
+    print('ok')
+
+if 10 in [10, 20, 30]:
+    print('ok')
+
+if -0.0:
+    print('yes')  # 任何值为 0 的数字都是 False
+
+if [1, 2]:
+    print('yes')  # 非空对象都是 True
+
+if ' ':
+    print('yes')  # 空格字符也是字符，条件为 True
+
+
+
