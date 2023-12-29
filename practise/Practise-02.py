8e3907410cc41716ba431b763cf9f5bee0c610e7
jlin <jlin@justhealth.cn>
2023-12-29 16:15:28 +0800

    根据出生日期计算年龄

--- practise/Practise-02.py
+++ practise/Practise-02.py
@@ -5,3 +5,5 @@ print('hello' + 'world!')  # 加号表示字符拼接 helloworld!
 print('hello', 'world', sep='***')  # 单词间用***分隔 hello***world
 print('#' * 50)  # *号表示重复 50 遍
 print('how are you?', end='')  # 默认 print 会打印回车，end=''表示不要回车
+print()
+print("this ""is ""string")
