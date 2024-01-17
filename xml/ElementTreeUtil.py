418cf6c4ec1404398543c200a3e3deda4c57f399
jlin <jlin@justhealth.cn>
2024-01-17 09:36:07 +0800

    parse xml

--- xml/ElementTreeUtil.py
+++ xml/ElementTreeUtil.py
@@ -15,7 +15,35 @@ print('\n')
 # 打印root的child层的tag和attrib
 for child in root:
    print(child.tag, child.attrib)
-
 # 查找find()
 print(root.find('R1'))
+print('------------------------')
 
+tree = ET.parse('lab2.xml')
+root = tree.getroot()
+itemList = root.find('Items').findall('Item')
+
+for item in root.find('Items').iter('Item'):
+   if item.find('PATIENT_NAME') != None:
+      print(item.find('PATIENT_NAME').text)
+   if item.find('INPATIENT_ID') != None:
+      print(item.find('INPATIENT_ID').text)
+   if item.find('CHECK_TIME') != None:
+      print(item.find('CHECK_TIME').text)
+   if item.find('INSPECTION_ID') != None:
+      print(item.find('INSPECTION_ID').text)
+   if item.find('TEST_ORDER_NAME') != None:
+      print(item.find('TEST_ORDER_NAME').text)
+   if item.find('QUANTITATIVE_RESULT') != None:
+      print(item.find('QUANTITATIVE_RESULT').text)
+   if item.find('TEST_ITEM_ID') != None:
+      print(item.find('TEST_ITEM_ID').text)
+   if item.find('CHINESE_NAME') != None:
+      print(item.find('CHINESE_NAME').text)
+   if item.find('TEST_ITEM_UNIT') != None:
+      print(item.find('TEST_ITEM_UNIT').text)
+   if item.find('TEST_ITEM_REFERENCE') != None:
+      print(item.find('TEST_ITEM_REFERENCE').text)
+   if item.find('QUALITATIVE_RESULT') != None:
+      print(item.find('QUALITATIVE_RESULT').text)
+   print()
