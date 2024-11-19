c433dd61d20cab8c6310d6b41f4e2508e1c7f6c2
jlin <jlin@justhealth.cn>
2024-11-19 10:07:03 +0800

    Cycle

---
+++ basic/Cycle.py
@@ -0,0 +1,11 @@
+for letter in 'Python':
+    print(letter)
+
+print()
+
+fruits = ['banana', 'apple', 'mango']
+for index in range(len(fruits)):
+    print(fruits[index])
+else:
+    print("done!")
+
