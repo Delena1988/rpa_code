acf27bc5ccc8d018b7c924f51d0dac66da60ad9f
jlin <jlin@justhealth.cn>
2024-11-27 09:54:20 +0800

    Regular Expression - findall

--- basic/Re.py
+++ basic/Re.py
@@ -32,6 +32,7 @@ num = re.sub(r'\D', "", phone)
 print("电话号码是 : ", num)
 print()
 
+print("=============group=============")
 text = "abc123def456"
 # 每个分组都可以通过小括号括起来，用于将匹配结果分成多个组，可以通过在group方法中指定分组编号来获取指定分组的匹配结果。
 pattern = re.compile(r'(\d+)(\w+)')
@@ -43,9 +44,19 @@ print("group1: ", match.group(1))
 print("group2: ", match.group(2))
 print()
 
+print("=============start、end、span=============")
 # 从'1'的位置开始匹配，正好匹配
 pattern = re.compile(r'\d+')
 m = pattern.match('one12twothree34four', 3, 10)
 print('start: ', m.start(0))
 print('end: ', m.end(0))
 print('span: ', m.span(0))
+print()
+
+print("=============findall=============")
+pattern = re.compile(r'\d+')  # 查找数字
+result1 = pattern.findall('runoob 123 google 456')
+result2 = pattern.findall('run88oob123google456', 0, 10)
+print(result1)
+print(result2)
+print()
