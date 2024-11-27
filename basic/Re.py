4a47b5dfb6733d5280d136ff53d5f424c4ac53f9
jlin <jlin@justhealth.cn>
2024-11-27 10:06:53 +0800

    Regular Expression - finditer

--- basic/Re.py
+++ basic/Re.py
@@ -60,3 +60,9 @@ result2 = pattern.findall('run88oob123google456', 0, 10)
 print(result1)
 print(result2)
 print()
+
+print("=============finditer=============")
+it = re.finditer(r"\d+", "12a32bc43jf3")
+for match in it:
+    print(match.group())
+print()
