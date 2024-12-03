91ec83f1f70e664f44527109a375d3b59136ff8a
jlin <jlin@justhealth.cn>
2024-12-03 18:19:03 +0800

    PostgreSQL create、delete tabel

---
+++ advance/pgsql/deleteTable.py
@@ -0,0 +1,14 @@
+import psycopg2
+# 获得连接
+conn = psycopg2.connect(database="python", user="postgres", password="123456", host="127.0.0.1", port="5432")
+# 获得游标对象，一个游标对象可以对数据库进行执行操作
+cursor = conn.cursor()
+# sql语句 建表
+sql = """DROP TABLE student;"""
+# 执行语句
+cursor.execute(sql)
+print("student table created successfully")
+# 事物提交
+conn.commit()
+# 关闭数据库连接
+conn.close()
